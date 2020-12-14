59c90231d4bc832f0c1b6bf36a488c560e00b7c7
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-14 05:05:22 +0100

    add axios and update Airlines index component (root page)

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
-	return <div>This is the Airlines#index view for our app.</div>
+	const [airlines, setAirlines] = useState([])
+
+	useEffect(()=> {
+
+		axios.get('api/v1/airlines.json')
+		.then( resp => {
+			setAirlines(resp.data.data)
+		})
+		.catch( resp => console.log(resp) )
+	}, [airlines.length])
+
+	const list = airlines.map( item => {
+	return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+	})
+
+	return (
+	<Fragment>
+		<div>This is the Airlines#index view for our app.</div>
+		<ul>{list}</ul>
+	</Fragment>
+	)
 }
 
 export default Airlines
