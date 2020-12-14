a7601a45f768b0f4d4af83592896aefccd2bebc7
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-14 02:51:51 +0100

    fix errors in controllers with Insomnia testing

---
+++ app/controllers/api/V1/airlines_controller.rb
@@ -0,0 +1,68 @@
+module Api
+  module V1
+    class AirlinesController < ApplicationController
+    	protect_from_forgery with: :null_session
+      # GET /api/v1/airlines
+      def index
+        render json: AirlineSerializer.new(airlines, options).serializable_hash
+      end
+
+      # GET /api/v1/airlines/:slug
+      def show
+        render json: AirlineSerializer.new(airlines, options).serializable_hash
+      end
+
+      # POST /api/v1/airlines
+      def create
+        airline = Airline.new(airline_params)
+
+        if airline.save
+          render json: AirlineSerializer.new(airline).serializable_hash
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      # PATCH /api/v1/airlines/:slug
+      def update
+        airline = Airline.find_by(slug: params[:slug])
+
+        if airline.update(airline_params)
+          render json: AirlineSerializer.new(airline, options).serializable_hash
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      # DELETE /api/v1/airlines/:slug
+      def destroy
+        if airline.destroy
+          head :no_content
+        else
+          render json: { errors: airline.errors.messages }, status: 422
+        end
+      end
+
+      private
+      # Used For compound documents with fast_jsonapi
+      def options
+        @options ||= { include: %i[reviews] }
+      end
+
+      # Get all airlines
+      def airlines
+        @airlines ||= Airline.all
+      end
+
+      # Get a specific airline
+      def airline
+        @airline ||= Airline.find_by(slug: params[:slug])
+      end
+
+      # Strong params
+      def airline_params
+        params.require(:airline).permit(:name, :image_url)
+      end
+    end
+  end
+end 
---
+++ app/controllers/api/V1/reviews_controller.rb
@@ -0,0 +1,35 @@
+module Api
+  module V1
+    class ReviewsController < ApplicationController
+    	protect_from_forgery with: :null_session
+      # POST /api/v1/reviews
+      def create
+        review = Review.new(review_params)
+
+        if review.save
+          render json: ReviewSerializer.new(review).serializable_hash
+        else
+          render json: { errors: review.errors.messages }, status: 422
+        end
+      end
+
+      # DELETE /api/v1/reviews/:id
+      def destroy
+        review = Review.find(params[:id])
+
+        if review.destroy
+          head :no_content
+        else
+          render json: { errors: review.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      # Strong params
+      def review_params
+        params.require(:review).permit(:title, :description, :score, :airline_id)
+      end
+    end
+  end
+end 
