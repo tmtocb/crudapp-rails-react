7ea620229b3807b48f22a966c668c3869e949f22
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-14 06:27:26 +0100

    add styled-component and style Airline Homepage and Airline invidual component

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,35 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import Airline from './Airline'
+import styled from 'styled-components'
+
+const Home = styled.div` 
+	text-align: center;
+	max-width: 1200px;
+	margin-left: auto;
+	margin-right: auto;
+`
+
+const Header = styled.div`
+	padding: 100px 100px 10px 100px;
+	
+	h1 {
+		font-size: 42px;
+	}
+`
+
+const Subheader = styled.div`
+	font-weight: 300;
+	font-size: 26px;
+`
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	grid-gap: 20px;
+	width: 100%;
+	padding: 20px;
+`
+
 
 const Airlines = () => {
 	const [airlines, setAirlines] = useState([])
@@ -26,16 +55,16 @@ const grid = airlines.map( item => {
 
 	return (
 
-	<div className="home">
-		<div className="header">
+	<Home>
+		<Header>
 			<h1>Open Flights</h1>
-			<div className="subheader"> Honest, unbiased reviews
-			</div>
-		</div>
-		<div className="grid">
+			<Subheader> Honest, unbiased reviews
+			</Subheader>
+		</Header>
+		<Grid>
 			{grid}
-		</div>
-	</div>
+		</Grid>
+	</Home>
 	)
 }
 
